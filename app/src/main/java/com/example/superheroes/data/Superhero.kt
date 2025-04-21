fa41fcca25b09f1e792e1a69c145fa63a2838911
oscarAndroid <oscarlimachitapia@gmail.com>
2025-04-21 20:03:42 +0200

    busqueda lista Empieza: -Activity detail -carta con un menu-bar de 3 iconos - a su vez tendra un contenido

--- app/src/main/java/com/example/superheroes/data/Superhero.kt
+++ app/src/main/java/com/example/superheroes/data/Superhero.kt
@@ -1,12 +1,35 @@
 package com.example.superheroes.data
 
+import com.google.gson.annotations.SerializedName
+
 
 data class Superhero(
     val id: String,
     val name: String,
-    val image: Image)
+    val image: Image,
+    val biography: Biography,
+    @SerializedName("powerstats") val stats: Stats
+    )
+
+
 data class Image(
     val url: String
+)
+data class Biography(
+    @SerializedName("full-name") val realName: String,
+    @SerializedName("place-of-birth") val placeOfBirth: String,
+    val publisher: String,
+    val alignment: String
+)
+
+class Stats(
+    var intelligence: String,
+    var strength: String,
+    var speed: String,
+    var durability: String,
+    var power: String,
+    var combat:String
+
 )
 data class SuperheroSearchResponse(
     val results: List<Superhero>
