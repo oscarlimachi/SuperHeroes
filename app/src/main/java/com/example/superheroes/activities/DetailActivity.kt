e9d76bb200c2aa20976aa7e39b55efe394d76aa1
oscarAndroid <oscarlimachitapia@gmail.com>
2025-04-23 19:39:55 +0200

    Acabado

--- app/src/main/java/com/example/superheroes/activities/DetailActivity.kt
+++ app/src/main/java/com/example/superheroes/activities/DetailActivity.kt
@@ -41,27 +41,36 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val id = intent.getStringExtra(SUPERHERO_ID)!!
         getSuperheroById(id)
 
         //invocar a la navegacion y recibir pulsar
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem->
-            binding.contentBiography.visibility = View.GONE
-            binding.contentAppearance.visibility = View.GONE
-            binding.contentStats.visibility = View.GONE
+            binding.contentBiography.root.visibility = View.GONE
+            binding.contentAppearance.root.visibility = View.GONE
+            binding.contentPowerStats.root.visibility = View.GONE
 
             when(menuItem.itemId){
-                R.id.menuBiography -> binding.contentBiography.visibility = View.VISIBLE
-                R.id.menuAppearance -> binding.contentAppearance.visibility = View.VISIBLE
-                R.id.menuStats -> binding.contentStats.visibility = View.VISIBLE
+                R.id.menuBiography -> binding.contentBiography.root.visibility = View.VISIBLE
+                R.id.menuAppearance -> binding.contentAppearance.root.visibility = View.VISIBLE
+                R.id.menuStats -> binding.contentPowerStats.root.visibility = View.VISIBLE
             }
             true
         }
 
         /*forzamos para a pulsar click para que se oculte*/
         binding.bottomNavigationView.selectedItemId = R.id.menuBiography
-
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
     fun getSuperheroById(id: String){
         CoroutineScope(Dispatchers.IO).launch {
@@ -84,13 +93,42 @@ class DetailActivity : AppCompatActivity() {
         Picasso.get().load(superhero.image.url).into(binding.avatarImageView)
 
         //Biography
-        binding.publisherTextView.text = superhero.biography.publisher
-        binding.placeOfBirthTextView.text = superhero.biography.placeOfBirth
-        binding.alignmentTextView.text = superhero.biography.alignment
+        binding.contentBiography.publisherTextView.text = superhero.biography.publisher
+        binding.contentBiography.placeOfBirthTextView.text = superhero.biography.placeOfBirth
+        binding.contentBiography.alignmentTextView.text = superhero.biography.alignment
+        binding.contentBiography.alignmentTextView.setTextColor(getColor(superhero.getAlignmentColor()))
+        binding.contentBiography.occupationTextView.text = superhero.work.occupation
+        binding.contentBiography.baseTextView.text = superhero.work.base
         //Appearance
+        with(superhero.appearance){
+            binding.contentAppearance.genderTextView.text = gender
+            binding.contentAppearance.raceTextView.text = race
+            binding.contentAppearance.eyeColorTextView.text = eyeColor
+            binding.contentAppearance.hairColorTextView.text = hairColor
+            binding.contentAppearance.heightTextView.text = height[1]
+            binding.contentAppearance.weightTextView.text = weight[1]
+        }
         //Stats
-        binding.intelligenceTextView.text = "${superhero.stats.intelligence.toIntOrNull() ?: 0}"
-        binding.intelligenceProgress.progress = superhero.stats.intelligence.toIntOrNull() ?: 0
+        with(superhero.stats){
+            binding.contentPowerStats.intelligenceTextView.text = "${intelligence}"
+            binding.contentPowerStats.intelligenceProgress.progress = intelligence
+
+            binding.contentPowerStats.strengthTextView.text = "${strength}"
+            binding.contentPowerStats.strengthProgress.progress = strength
+
+            binding.contentPowerStats.speedTextView.text = "${speed}"
+            binding.contentPowerStats.speedProgress.progress = speed
+
+            binding.contentPowerStats.durabilityTextView.text = "${durability}"
+            binding.contentPowerStats.durabilityProgress.progress = durability
+
+            binding.contentPowerStats.powerTextView.text = "${power}"
+            binding.contentPowerStats.powerProgress.progress = power
+
+            binding.contentPowerStats.combatTextView.text = "${combat}"
+            binding.contentPowerStats.combatProgress.progress = combat
+
+        }
 
     }
 
--- app/src/main/java/com/example/superheroes/data/Superhero.kt
+++ app/src/main/java/com/example/superheroes/data/Superhero.kt
@@ -1,6 +1,14 @@
 package com.example.superheroes.data
 
+import android.os.WorkSource
+import androidx.annotation.ColorRes
+import com.example.superheroes.R
+import com.google.android.material.resources.TextAppearance
+import com.google.gson.TypeAdapter
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
 
 
 data class Superhero(
@@ -8,8 +16,19 @@ data class Superhero(
     val name: String,
     val image: Image,
     val biography: Biography,
+    val work: Work,
+    val appearance: Appearance,
     @SerializedName("powerstats") val stats: Stats
-    )
+    ){
+    @ColorRes
+    fun getAlignmentColor() : Int {
+        return when(biography.alignment){
+            "good" -> R.color.alignment_color_good
+            "bad" -> R.color.alignment_color_bad
+            else -> R.color.alignment_color_neutral
+        }
+    }
+}
 
 
 data class Image(
@@ -22,16 +41,47 @@ data class Biography(
     val alignment: String
 )
 
+data class Work(
+    val occupation: String,
+    val base: String
+)
 class Stats(
-    var intelligence: String,
-    var strength: String,
-    var speed: String,
-    var durability: String,
-    var power: String,
-    var combat:String
+    @JsonAdapter(IntegerAdapter::class) var intelligence: Int,
+    @JsonAdapter(IntegerAdapter::class) var strength: Int,
+    @JsonAdapter(IntegerAdapter::class) var speed: Int,
+    @JsonAdapter(IntegerAdapter::class) var durability: Int,
+    @JsonAdapter(IntegerAdapter::class) var power: Int,
+    @JsonAdapter(IntegerAdapter::class) var combat:Int
+)
+
+class Appearance(
+    val gender: String,
+    val race: String,
+    @SerializedName("eye-color")  val eyeColor: String,
+    @SerializedName("hair-color") val hairColor: String,
+    val height: List<String>,
+    val weight: List<String>
+
+
+
 
 )
 data class SuperheroSearchResponse(
     val results: List<Superhero>
 )
+
+class IntegerAdapter : TypeAdapter<Int>() {
+    override fun write(out: JsonWriter?, value: Int) {
+        out?.value(value)
+    }
+
+    override fun read(`in`: JsonReader?): Int {
+        return try {
+            `in`!!.nextString()!!.toInt()
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+}
 
